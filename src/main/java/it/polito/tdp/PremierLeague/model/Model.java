6f896a83c997b8939bf08137b4e2c14ff3ff1297
valesarza <valesarza@gmail.com>
2022-06-06 11:25:32 +0200

    Popolata la idMap

--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -1,6 +1,27 @@
 package it.polito.tdp.PremierLeague.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
 
 public class Model {
+	
+	private PremierLeagueDAO dao;
+	private Graph<Team, DefaultWeightedEdge> grafo;
+	private Map<Integer, Team> idMap;
+	
+	public Model() {
+		this.dao = new PremierLeagueDAO();
+		this.idMap = new HashMap<>();
+		this.dao.getAllTeams(idMap);
+	}
+	
+	public void CreaGrafo() {
+		
+	}
 	
 }
